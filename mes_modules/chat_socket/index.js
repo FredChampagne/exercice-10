e8b25ffe9132ee46371341d4b40f7bbd60ae671d
Frédéric Champagne <fred.champagne@live.ca>
2018-03-08 16:25:55 -0500

    Avancement chat

--- mes_modules/chat_socket/index.js
+++ mes_modules/chat_socket/index.js
@@ -1,29 +1,30 @@
 const util = require('util');
 let socketio = require('socket.io')
 
-module.exports.listen = function(server){
-    let io = socketio.listen(server)
+module.exports.listen = function (server) {
+  let io = socketio.listen(server)
 
-    // Traitement du socket
-    let objUtilisateur = {}
-    io.on('connection', function(socket){
-      console.log(socket.id);
-      socket.on('setUser', function(data) {
-        objUtilisateur[socket.id] = data.user;
-        console.log("objUtilisateur = " + util.inspect(objUtilisateur));
-        console.log(util.inspect(data));
-        socket.emit('valide_user',data);
-        io.sockets.emit('diffuser_list_user', objUtilisateur);
-      });
-      // Traitement des messages
-      socket.on('setMessage', function(data){
-        socket.broadcast.emit('diffuser_message', data.message);
-        socket.emit('valide_message', data.message);
-      })
-      socket.on('disconnect', function(){
-        delete objUtilisateur[socket.id];
-        io.sockets.emit('diffuser_list_user', objUtilisateur);
-      });
-   });
- return io;
+  // Traitement du socket
+  let objUtilisateur = {}
+  io.on('connection', function (socket) {
+    console.log(socket.id);
+    socket.on('setUser', function (data) {
+      objUtilisateur[socket.id] = data.user;
+      console.log("objUtilisateur = " + util.inspect(objUtilisateur));
+      console.log(util.inspect(data));
+      socket.emit('valide_user', data);
+      io.sockets.emit('diffuser_list_user', objUtilisateur);
+    });
+    // Traitement des messages
+    socket.on('setMessage', function (data) {
+      console.log(util.inspect(objUtilisateur[socket.id]));
+      socket.broadcast.emit('diffuser_message', data.message);
+      socket.emit('valide_message', data.message);
+    })
+    socket.on('disconnect', function () {
+      delete objUtilisateur[socket.id];
+      io.sockets.emit('diffuser_list_user', objUtilisateur);
+    });
+  });
+  return io;
 }

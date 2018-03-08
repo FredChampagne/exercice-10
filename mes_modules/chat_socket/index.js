8e4f03625b81d2b46e98bedd9e87f1eccc0c21d5
Champagne1 <e1201583@cmaisonneuve.qc.ca>
2018-03-08 10:19:34 -0500

    Avancement du chat

--- mes_modules/chat_socket/index.js
+++ mes_modules/chat_socket/index.js
@@ -1,13 +1,25 @@
+const util = require('util');
 let socketio = require('socket.io')
 
 module.exports.listen = function(server){
     let io = socketio.listen(server)
 
-    // ------------------------------ Traitement du socket
+    // Traitement du socket
     let objUtilisateur = {}
     io.on('connection', function(socket){
-    console.log(socket.id)
-    // .......
+      console.log(socket.id);
+      socket.on('setUser', function(data) {
+        objUtilisateur[socket.id] = data.user;
+        console.log("objUtilisateur = " + util.inspect(objUtilisateur));
+        console.log(util.inspect(data));
+        socket.emit('valide_user',data);
+        io.sockets.emit('diffuser_list_user', objUtilisateur);
+      });
+      // Traitement des messages
+      socket.on('setMessage', function(data){
+        socket.broadcast.emit('diffuser_message', data.message);
+        socket.emit('valide_message', data.message);
+      })
    });
  return io;
 }
